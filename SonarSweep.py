ae0953627ed1de4b698f2c13b97d303982ff8fdc
bAe <bjekele@fexco.com>
2021-12-16 19:05:12 +0000

    Boilerplate

---
+++ SonarSweep.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""SonarSweep.py
+    Author: Bissallah Ekele - bAe
+    Date: 16/12/2021
+    Description: Depth measurement.
+"""
+
+def get_part_1(depth_list):
+    """Count increasing depths
+
+        Keyword argument:
+        depth_list -- list of measured depths
+
+        Return:
+        increased depth count
+    """
+    return depth_list
+
+def get_part_2(depth_list):
+    pass
+
+def main():
+    test_input = "test-input"
+    puzzle_input = "puzzle-input"
+
+    file_name = test_input
+
+    with open(file_name) as f:
+        depth_list = f.read().splitlines()
+
+    print("Day_1 Part_1: " 
+        + str(get_part_1(depth_list)) 
+        + "\nPart_2: "
+        + str(get_part_2(depth_list)))
+
+if __name__ == "__main__":
+    main()
