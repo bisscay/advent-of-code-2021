5c1472c16f76c0097ba35c006a4e9bc6c9b9f45d
bAe <bjekele@fexco.com>
2021-12-16 19:49:44 +0000

    Part 2

--- Day1/SonarSweep.py
+++ Day1/SonarSweep.py
@@ -16,21 +16,50 @@ def get_part_1(depth_list):
     """
     count = 0
     for index in range(len(depth_list)-1):
-        if int(depth_list[index]) < int(depth_list[index+1]):
+        if depth_list[index] < depth_list[index+1]:
             count += 1
     return count
 
+def compute_sliding_window(depth_list):
+    """Derive sliding window sums
+
+        Keyword argument:
+        depth_list -- list of measured depths
+
+        Return:
+        list of sliding window sums
+    """
+    window_list =  [] # TODO: Define list size
+    for index in range(2, len(depth_list)):
+        window_list.append(
+            depth_list[index-2] + depth_list[index-1]
+            + depth_list[index]
+        )
+    return window_list
+
 def get_part_2(depth_list):
-    pass
+    """Count increasing depths on sliding window sums
+
+        Keyword argument:
+        depth_list -- list of measured depths
+
+        Return:
+        increased depth count
+    """
+    window_list = compute_sliding_window(depth_list)
+    return get_part_1(window_list)
 
 def main():
     test_input = "test-input"
     puzzle_input = "puzzle-input"
 
-    file_name = test_input
+    file_name = puzzle_input
 
     with open(file_name) as f:
         depth_list = f.read().splitlines()
+
+    for index, character in enumerate(depth_list):
+        depth_list[index] = int(character)
 
     print("Day_1 Part_1: " 
         + str(get_part_1(depth_list)) 
