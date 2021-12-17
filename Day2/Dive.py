a5aed01c3e43365a742f215745767319359d23df
bAe <bjekele@fexco.com>
2021-12-17 11:14:28 +0000

    Part 1

--- Day2/Dive.py
+++ Day2/Dive.py
@@ -2,28 +2,45 @@
 """SonarSweep.py
     Author: Bissallah Ekele - bAe
     Date: 17/12/2021
-    Description: Depth measurement.
+    Description: Position measurement.
 """
 
-def get_part_1():
-    pass
+def get_part_1(course_list):
+    """Multiply depth by horizontal position
 
-def get_part_2():
+        Keyword argument:
+        course_list -- list of planned course
+
+        Return:
+        Product of depth by horizontal position
+    """
+    forward = 0
+    depth = 0
+    for course in course_list:
+        if course[0] == "f":
+            forward += int(course[8:])
+        if course[0] == "d":
+            depth += int(course[5:])
+        if course[0] == "u":
+            depth -= int(course[3:])
+    return forward * depth
+
+def get_part_2(course_list):
     pass
 
 def main():
     test_input = "test-input"
     puzzle_input = "puzzle-input"
 
-    file_name = test_input
+    file_name = puzzle_input
 
     with open(file_name) as f:
-        input_list = f.read().splitlines()
+        course_list = f.read().splitlines()
 
     print("Day_1 Part_1: " 
-        + str(get_part_1(input_list)) 
+        + str(get_part_1(course_list)) 
         + "\nPart_2: "
-        + str(get_part_2(input_list)))
+        + str(get_part_2(course_list)))
 
 if __name__ == "__main__":
     main()
